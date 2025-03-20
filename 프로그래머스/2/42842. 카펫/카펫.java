444f29c08866f70701402be66b7d38c2e98765ec
peachea27 <peachea27@gmail.com>
2025-03-20 21:55:18 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int total = brown + yellow; 
+
+        for (int h = 3; h <= Math.sqrt(total); h++) {
+            if (total % h == 0) {
+                int w = total / h; 
+
+                if ((w - 2) * (h - 2) == yellow) {
+                    answer[0] = w; 
+                    answer[1] = h; 
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
