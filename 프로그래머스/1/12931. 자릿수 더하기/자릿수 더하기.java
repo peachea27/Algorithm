4ed8d917741824a86a462367b9eacc2490cfc171
peachea27 <peachea27@gmail.com>
2025-11-18 22:37:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,15 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        // 문자로 변경해서 모두 배열에 넣기
-        String charN = Integer.toString(n);
-        char[] array = charN.toCharArray();
-        System.out.println(array);
-        for(int i = 0; i < array.length; i++){
-            int number = (int)array[i] - '0';
-            answer += number;
+        String str = Integer.toString(n);
+        for(int i = 0; i <str.length(); i++) {
+            answer += str.charAt(i)-'0';
         }
-        
         
 
         return answer;
