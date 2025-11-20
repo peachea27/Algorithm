6d92c9693c73a28105fae4d946de339ceb8ab8aa
peachea27 <peachea27@gmail.com>
2025-11-20 23:08:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
         int sum = 0;
-        String xStr =Integer.toString(x);
-        for(int i = 0; i <xStr.length(); i++){
-          int xNum = Character.getNumericValue(xStr.charAt(i));
-          sum += xNum;
-        }
-        if(x % sum != 0) return false;
-        
-        return answer;
+        int num = x;
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }      
+        return (x % sum == 0);
     }
 }
